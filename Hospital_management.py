7f6e3428ba730132030ca7e0912ba3afcc5a2d52
sree90274-maker <sree90274@gmail.com>
2026-06-06 13:19:41 +0530

    hospital_management_system

--- Hospital_management.py
+++ Hospital_management.py
@@ -1,52 +1,43 @@
-patients = []
-appointments = []
-doctors = ['Dr.usha', 'Dr.Durga']
-#Register Patient
 def required_details():
-    patient_name = input("Enter Patient Name: ")
-    patient_age = int(input("Enter Patient Age: "))
-    patient_gender = input("Enter Patient Gender: ")
-    patient_weight = int(input("Enter Patient Weight: "))
-    patient_disease = input("Enter Patient Disease: ")
-    patient_address = input("Enter Patient Address: ")
-    patient_appointment = input("Enter Appointment Time: ")
+    patient_name=input("Enter Patient Name: ")
+    patient_age=int(input("Enter Patient Age: "))
+    patient_gender=input("Enter Patient Gender: ")
+    patient_weight=int(input("Enter Patient Weight: "))
+    patient_disease=input("Enter Patient Disease: ")
+    patient_address=input("Enter Patient Address: ")
     print(f"\n{'GOVERNMENT HOSPITAL':^80}")
     print(f"{'12-25, CHILAKAVARIPALLI, CTM, MADANAPALLE, A.P 517319':^80}")
-    print(f"{str(doctors):^80}")
+    print(f"{'Dr.usha,Dr.Durga':^80}")
     print("_" * 80)
-    print("Patient Name:", patient_name)
-    print("Patient Age:", patient_age)
-    print("Patient Gender:", patient_gender)
-    print("Patient Weight:", patient_weight)
-    print("Patient Disease:", patient_disease)
-    print("Patient Address:", patient_address)
-    print("Appointment Time:", patient_appointment)
-    print("\nPatient Registered Successfully!\n")
+    print("Patient Name:",patient_name)
+    print("Patient Age:",patient_age)
+    print("Patient Gender:",patient_gender)
+    print("Patient Weight:",patient_weight)
+    print("Patient Disease:",patient_disease)
+    print("Patient Address:",patient_address)
+    print("\nPatient Registered Successfully\n")
 required_details()
 #Schedule Appointment
 def schedule_appointment():
-    name = input("Enter Patient Name: ")
-    time = input("Enter Appointment Time: ")
-    appointments.append({"name": name,"time": time})
+    name=input("Enter Patient Name:")
+    time=input("Enter Appointment Time:")
+    print("name:",name)
+    print("time:",time)
     print("Appointment Scheduled Successfully")
 #Assign Doctor
 def assign_doctor():
-    name = input("Enter Patient Name: ")
+    name=input("Enter Patient Name:")
     print("\nAvailable Doctors:")
-    for doctor in doctors:
-        print(doctor)
-    doctor_name = input("Choose Doctor: ")
+    doctor_name = input("Choose Doctor:")
     print("\nDoctor Assigned Successfully")
     print("Patient:",name)
     print("Doctor:",doctor_name)
 #Calculate Hospital Bill
 def calculate_bill():
-    consultation_fee = 500
-    medicine_fee = int(input("Enter Medicine Fee: "))
-    test_fee = int(input("Enter Test Fee: "))
-    total_bill = consultation_fee + medicine_fee + test_fee
+    medicine_fee=int(input("Enter Medicine Fee:"))
+    test_fee=int(input("Enter Test Fee: "))
+    total_bill=medicine_fee + test_fee
     print("\n----- BILL -----")
-    print("Consultation Fee:",consultation_fee)
     print("Medicine Fee:",medicine_fee)
     print("Test Fee:",test_fee)
     print("Total Bill:",total_bill)
@@ -58,7 +49,7 @@ while True:
     print("3.Assign Doctor")
     print("4.Calculate Bill")
     print("5.Exit")
-    menu=int(input("Enter Choice: "))
+    menu=int(input("Enter Choice:"))
     if menu==1:
         required_details()
     elif menu==2:
